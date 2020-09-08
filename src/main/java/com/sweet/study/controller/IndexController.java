5b631149d20b12016b5e1ffe1fe31064b05d451e
SAILUNTIRE\guang.wang <guang.wang@sailuntire.com>
2020-09-08 17:23:11 +0800

    初始化

--- src/main/java/com/sweet/study/controller/IndexController.java
+++ src/main/java/com/sweet/study/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.sweet.study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author guang.wang
@@ -14,5 +15,11 @@ public class IndexController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/aa")
+    @ResponseBody
+    public String aa(){
+        return "aa";
     }
 }
